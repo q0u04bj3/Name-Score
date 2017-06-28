71aa019d905352814fbb6a3bb69de0f11b930318
Rita <q0u04bj3@gmail.com>
2017-06-28 11:48:04 +0800

    update

--- NameScore.py
+++ NameScore.py
@@ -1,9 +1,19 @@
+"""
 file = ["CDEF","ABC","FIJK"]
 file = sorted(file)
+"""
+
+import re
+pat = '[A-Z]+'
+
+name_file = open("name.dat","r")
+names_file = name_file.read()
+file = sorted(re.findall(pat, names_file))
+
 
 for index, element in enumerate(file):
     sum=0
     for letter in element:
         sum+=(ord(letter)-64)
 
-    print sum*(index+1)
+        print sum*(index+1)
