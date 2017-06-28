b756163b52d3dec14ac40a2c4c2c79be2084926c
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-28 13:17:43 +0800

    Update NameScore.py

--- NameScore.py
+++ NameScore.py
@@ -5,6 +5,5 @@ name_file = open("name.dat","r")
 names_file = name_file.read()
 file = sorted(re.findall(pat, names_file))
 
-
 for index, element in enumerate(file):
     print sum([ord(letter)-64 for letter in element])*(index+1)
